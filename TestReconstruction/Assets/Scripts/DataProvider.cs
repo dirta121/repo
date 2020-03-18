471ffcf5deea4f89c0de937c80269cd6ba680769
dirta121 <spekt7035@yandex.ru>
2020-03-18 19:58:05 +0300

    Added TestReconstruction

---
+++ TestReconstruction/Assets/Scripts/DataProvider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using Newtonsoft.Json;
+using System.Linq;
+
+public class DataProvider : MonoBehaviour
+{
+    static Data[] ReadData()
+    {
+        var json = ReadString();
+
+       return JsonConvert.DeserializeObject<Data[]>(json);
+    }
+
+   public static Data[] GetData()
+    {
+        return ReadData();
+    }
+
+    static string ReadString()
+    {
+        using (StreamReader reader = new StreamReader("Assets/Resources/reconstruction_description.json"))
+            return reader.ReadToEnd();
+    }
+}
+
---
+++ TestReconstruction/Assets/Scripts/Reconstruction.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Newtonsoft.Json;
+
+public class Data
+{
+    [JsonProperty("reconstruction")]
+    public Reconstruction reconstruction { get; set; }
+}
+[Serializable]
+public partial class Reconstruction
+{
+    [JsonProperty("hash")]
+    public string Hash { get; set; }
+
+    [JsonProperty("images")]
+    public ImageElement[] Images { get; set; }
+
+    [JsonProperty("comments")]
+    public string Comments { get; set; }
+
+    [JsonProperty("description")]
+    public Description Description { get; set; }
+
+    [JsonProperty("images_size")]
+    public long ImagesSize { get; set; }
+
+    [JsonProperty("images_number")]
+    public long ImagesNumber { get; set; }
+
+    [JsonProperty("reconstruction_id")]
+    public long ReconstructionId { get; set; }
+}
+[Serializable]
+public partial class Description
+{
+    [JsonProperty("skip")]
+    public bool Skip { get; set; }
+}
+[Serializable]
+public partial class ImageElement
+{
+    [JsonProperty("image")]
+    public ImageImage Image { get; set; }
+}
+[Serializable]
+public partial class ImageImage
+{
+    [JsonProperty("hash")]
+    public string Hash { get; set; }
+
+    [JsonProperty("image_id")]
+    public long ImageId { get; set; }
+
+    [JsonProperty("camera_description")]
+    public CameraDescription CameraDescription { get; set; }
+}
+[Serializable]
+public partial class CameraDescription
+{
+    [JsonProperty("position")]
+    public float[] Position { get; set; }
+
+    [JsonProperty("orientation")]
+    public float[] Orientation { get; set; }
+}
