2ed3ee48a4f76b853ec26213fe7710b65b8e7acd
Mikhail Valishevskiy <spekt7035@yandex.ru>
2020-10-09 18:31:44 +0300

    Add testBuild
    add build

---
+++ TestTenkyu/Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Quit();
+        }
+    }
+    private void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Quiting");
+    }
+}
